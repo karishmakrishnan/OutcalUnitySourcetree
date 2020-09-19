c47016e9391547f0e31081edf71e6a35b4986379
karishmakrishnan <karishmakrishnan321@gmail.com>
2020-09-19 22:26:25 +0530

    2D Assignment

---
+++ 2D UI/2dUI/Assets/Scripts/UIScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+  public void MySceneLoader(int sceneIndex){
+            SceneManager.LoadScene(sceneIndex);
+  }
+}
---
+++ MyFirst2D/MyFirst2D/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake(){
+        Debug.Log("Myfirst scipt Awake!!!!!!!!!!!!!!!");
+    }
+    void Start()
+    {
+        Debug.Log("Myfirst scipt Start!!!!!!!!!!!!!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(  Input.GetMouseButtonDown(0)){
+          Debug.Log("My mouse left click");
+      }
+      if(  Input.GetMouseButtonDown(1)){
+          Debug.Log("My mouse right click");
+      }
+     if(Input.GetKeyDown(KeyCode.F)){
+         Debug.Log("Key is pressed!!!!!!!!!!!!");
+     }
+     if(Input.GetKey(KeyCode.F)){
+         Debug.Log("Input is F");
+     }
+     if(Input.GetKeyUp(KeyCode.F)){
+         Debug.Log("Key is released!!!!!!!!!!!");
+     }
+    } 
+    private void LateUpdate(){
+
+    }
+    void onDestroy(){
+        Debug.Log("Myfirst scipt Destoy!!!!!!!!!!!!!!!");
+    }
+}
